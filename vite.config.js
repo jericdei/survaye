f5b1a8854afeda5e85545a457c26692c8d4a7184
Jeric June Logan <loganjeric25@gmail.com>
2024-10-31 02:55:59 +0800

    rename project

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     hmr: {
-      host: "laravel-start.localhost",
+      host: "survaye.localhost",
       clientPort: import.meta.env.VITE_DEV_PORT ?? 5181,
     },
   },
